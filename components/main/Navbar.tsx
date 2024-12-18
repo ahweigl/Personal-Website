08e9cd5572e5d224ca7076384fbefd2d56cec1a1
Angela Weigl <ahweigl@gmail.com>
2024-12-18 13:55:44 -0700

    revised hero

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -7,13 +7,13 @@ const Navbar = () => {
         <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#1d4ed8]/50 bg-[#03001417 backdrop-blur-md z-50 px-10'>
             <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
                 <a href='#about-me' className='h-auto w-auto flex flex-row items-center'>
-                <Image src="/Sparkle.gif" alt="GIF Icon" width={30}
-                        height={30}
+                <Image src="/Sparkle.gif" alt="GIF Icon" width={80}
+                        height={80}
                         className='cursor-pointer'/>
 
-                    <span className='font-swell text-xl ml-[10px] hidden md:block text-gray-300'>
+                    {/* <span className='font-swell text-xl ml-[10px] hidden md:block text-gray-300'>
                         Angela Weigl
-                    </span>
+                    </span> */}
                 </a>
 
                 <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
